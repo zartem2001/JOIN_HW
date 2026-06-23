af5acc61f1b680e6afbe13c23d239c590f402764
zartem2001 <116199542+zartem2001@users.noreply.github.com>
2026-06-23 13:49:05 +0300

    first commit

---
+++ src/test/java/ru/netology/join_hw/JoinHwApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.netology.join_hw;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JoinHwApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
