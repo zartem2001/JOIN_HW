b14a3d48016e9fb3f77fdbedaae4a35bcd6c67cd
zartem2001 <116199542+zartem2001@users.noreply.github.com>
2026-06-26 14:50:54 +0300

    Обновлён ProductController

--- JOIN_HW-main/src/main/java/ru/netology/join_hw/ProductController.java
+++ JOIN_HW-main/src/main/java/ru/netology/join_hw/ProductController.java
@@ -1,6 +1,5 @@
 package ru.netology.join_hw;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
